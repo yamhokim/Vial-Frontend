b6e6f62762a05942d85c988583066e99d055282b
Yoonho Kim <CAKIMYOO@de.eu.sew>
2025-05-05 17:05:52 -0400

    created in progress dummy version of table using static data

---
+++ src/components/MedicationTable.tsx
@@ -0,0 +1,116 @@
+"use client";
+import "@mantine/core/styles.css";
+import { Checkbox, Table, Tooltip } from "@mantine/core";
+import { useState } from "react";
+import { GoPlus } from "react-icons/go";
+
+type Field = {
+  id: Number;
+  label: string;
+  value: string;
+  CRA: boolean;
+  DM: boolean;
+  query?: boolean;
+  error?: boolean;
+};
+
+const data: Field[] = [
+  {
+    id: 1,
+    label: "Were any medications/therapies taken?",
+    value: "Yes",
+    CRA: true,
+    DM: false,
+  },
+  {
+    id: 2,
+    label: "What was the medication/treatment/therapy name?",
+    value: "Ibuprofen",
+    CRA: false,
+    DM: false,
+  },
+  {
+    id: 3,
+    label: "What was the individual dose of the medication?",
+    value: "500",
+    CRA: true,
+    DM: true,
+  },
+  {
+    id: 4,
+    label: "What was the unit of the medication dose?",
+    value: "mg",
+    CRA: false,
+    DM: true,
+    query: true,
+  },
+  {
+    id: 5,
+    label: "Is the medication ongoing?",
+    value: "No",
+    CRA: true,
+    DM: false,
+  },
+];
+
+export default function MedicationTable() {
+  const [tableData, setTableData] = useState(data);
+
+  // Handle checkbox change
+  const handleCraChange = (id: Number) => {
+    setTableData((prevData) =>
+      prevData.map((item) =>
+        item.id === id ? { ...item, CRA: !item.CRA } : item
+      )
+    );
+  };
+
+  const handleDmChange = (id: Number) => {
+    setTableData((prevData) =>
+      prevData.map((item) =>
+        item.id === id ? { ...item, DM: !item.DM } : item
+      )
+    );
+  };
+
+  const rows = tableData.map((element) => (
+    <Table.Tr key={element.id}>
+      <Table.Td>{element.label}</Table.Td>
+      <Table.Td>{element.value}</Table.Td>
+      <Table.Td>
+        <Checkbox
+          checked={element.CRA}
+          onChange={() => handleCraChange(element.id)}
+        />
+      </Table.Td>
+      <Table.Td>
+        <Checkbox
+          checked={element.DM}
+          onChange={() => handleDmChange(element.id)}
+        />
+      </Table.Td>
+      <Table.Td>
+        <Tooltip label="Create Query">
+          <GoPlus />
+        </Tooltip>
+      </Table.Td>
+    </Table.Tr>
+  ));
+
+  const tableHeaders = (
+    <Table.Tr>
+      <Table.Th>Field Data</Table.Th>
+      <Table.Th></Table.Th>
+      <Table.Th>CRA</Table.Th>
+      <Table.Th>DM</Table.Th>
+      <Table.Th>Queries</Table.Th>
+    </Table.Tr>
+  );
+  return (
+    <Table striped highlightOnHover captionSide="top">
+      <Table.Caption>Vial Form</Table.Caption>
+      <Table.Thead>{tableHeaders}</Table.Thead>
+      <Table.Tbody>{rows}</Table.Tbody>
+    </Table>
+  );
+}
