5c19d11599f93eba1b8eeeae66ba75db5c310be6
Yoonho Kim <CAKIMYOO@de.eu.sew>
2025-05-06 10:31:27 -0400

    got rid of modal from app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,10 +9,8 @@ import React from "react";
 
 export default function RootLayout({
   children,
-  modal,
 }: {
   children: React.ReactNode;
-  modal: React.ReactNode;
 }) {
   return (
     <html lang="en" {...mantineHtmlProps}>
