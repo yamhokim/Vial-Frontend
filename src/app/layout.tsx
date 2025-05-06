3e8b94070aa60c3305f6841ff7a89b8f895eb5a2
Yoonho Kim <CAKIMYOO@de.eu.sew>
2025-05-06 15:19:39 -0400

    created a header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,11 @@ import {
   ColorSchemeScript,
   MantineProvider,
   mantineHtmlProps,
+  Box,
+  Group,
 } from "@mantine/core";
 import React from "react";
+import { FaVial } from "react-icons/fa";
 
 export default function RootLayout({
   children,
@@ -18,7 +21,17 @@ export default function RootLayout({
         <ColorSchemeScript />
       </head>
       <body>
-        <MantineProvider>{children}</MantineProvider>
+        <MantineProvider>
+          <Box p="md" bg="#f8f9fa">
+            <Group justify="space-between">
+              <h1 style={{ margin: 0, fontSize: 24 }}>
+                <FaVial /> Vial Queries
+              </h1>
+              <h3>Welcome, User</h3>
+            </Group>
+          </Box>
+          {children}
+        </MantineProvider>
       </body>
     </html>
   );
