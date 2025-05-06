4c200fdb4d934f56353b82b8a93b763436911142
Yoonho Kim <CAKIMYOO@de.eu.sew>
2025-05-06 08:49:59 -0400

    fixed modal component, achieved true overlay

--- src/app/@modal/(.)create/[id]/page.tsx
+++ src/app/@modal/(.)create/[id]/page.tsx
@@ -1,11 +1,29 @@
 "use client";
 
 import React from "react";
-import { Modal } from "./modal";
-import { useSearchParams } from "next/navigation";
+import { useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { Modal, Textarea, Button } from "@mantine/core";
 
 export default function ModalCreateQueryPage() {
+  const router = useRouter();
   const searchParams = useSearchParams();
   const label = searchParams.get("label");
-  return <Modal>{label}</Modal>;
+  const [queryText, setQueryText] = useState("");
+
+  return (
+    <Modal
+      opened={true}
+      onClose={() => router.back()}
+      title={`Create a Query | ${label}`}
+    >
+      <Textarea
+        label="Query"
+        value={queryText}
+        onChange={(e) => setQueryText(e.currentTarget.value)}
+      />
+      <Button>Cancel</Button>
+      <Button>Submit</Button>
+    </Modal>
+  );
 }
