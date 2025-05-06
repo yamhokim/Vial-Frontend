0123ceefe6099de74dbd4dd9835089a5f1b00052
Yoonho Kim <CAKIMYOO@de.eu.sew>
2025-05-06 10:32:23 -0400

    Updated the CreatQueryPage component to use the new Modal component

--- src/app/form-data/create/[id]/page.tsx
+++ src/app/form-data/create/[id]/page.tsx
@@ -1,7 +1,9 @@
 "use client";
-import { useRouter, useSearchParams } from "next/navigation";
-import { Modal, Textarea, Button } from "@mantine/core";
+
+import React from "react";
 import { useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { Modal, Textarea, Button, Group } from "@mantine/core";
 
 export default function CreateQueryPage() {
   const router = useRouter();
@@ -9,19 +11,48 @@ export default function CreateQueryPage() {
   const label = searchParams.get("label");
   const [queryText, setQueryText] = useState("");
 
+  const handleClose = () => {
+    router.back();
+  };
+
+  const handleSubmit = () => {
+    // Need to add code to handle the submission of data
+    handleClose();
+  };
+
   return (
     <Modal
       opened={true}
       onClose={() => router.back()}
       title={`Create a Query | ${label}`}
+      centered
+      size="lg"
+      styles={{
+        content: {
+          minHeight: "200px",
+        },
+        title: {
+          fontWeight: 700,
+        },
+      }}
     >
       <Textarea
-        label="Query"
         value={queryText}
         onChange={(e) => setQueryText(e.currentTarget.value)}
+        placeholder="Add a new remark"
+        minRows={8}
+        maxRows={8}
+        autosize
+        styles={{
+          input: {
+            overflowY: "auto",
+          },
+        }}
       />
-      <Button>Cancel</Button>
-      <Button>Submit</Button>
+      <Group mt="md">
+        <Button onClick={handleClose}>Cancel</Button>
+        <Button onClick={handleSubmit}>Create</Button>
+      </Group>
     </Modal>
   );
 }
