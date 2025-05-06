8cd178c0bd5b63b1e72f75d00079c589525c4748
Yoonho Kim <CAKIMYOO@de.eu.sew>
2025-05-06 10:39:39 -0400

    included temporary home page that has button that routes to table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+"use client";
+import { Button } from "@mantine/core";
+import { useRouter } from "next/navigation";
+
 export default function Page() {
-  return <h1>Welcome</h1>;
+  const router = useRouter();
+
+  return (
+    <>
+      <h1>Welcome to Vial</h1>;
+      <Button onClick={() => router.push(`/form-data`)}>Go to Table</Button>
+    </>
+  );
 }
