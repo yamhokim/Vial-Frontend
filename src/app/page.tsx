62b32fcafd2a6844f6af7a624dc2f52636573596
Yoonho Kim <CAKIMYOO@de.eu.sew>
2025-05-06 15:53:30 -0400

    created a placeholder login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,73 @@
 "use client";
-import { Button } from "@mantine/core";
 import { useRouter } from "next/navigation";
+import {
+  Card,
+  TextInput,
+  PasswordInput,
+  Button,
+  Title,
+  Stack,
+} from "@mantine/core";
+import { useState } from "react";
 
 export default function Page() {
   const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e: React.FormEvent) => {
+    e.preventDefault();
+    alert(`Logging in as ${email}`);
+    router.push(`/form-data`);
+  };
 
   return (
-    <>
-      <h1>Welcome to Vial</h1>
-      <Button onClick={() => router.push(`/form-data`)}>Go to Table</Button>
-    </>
+    <div
+      style={{
+        minHeight: "80vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Card
+        shadow="md"
+        padding="xl"
+        radius="md"
+        withBorder
+        style={{
+          minWidth: 400,
+          minHeight: 400,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
+      >
+        <form onSubmit={handleLogin}>
+          <Stack>
+            <Title order={2} align="center" mb="md">
+              Welcome to Vial
+            </Title>
+            <TextInput
+              label="Username"
+              placeholder="Enter Username"
+              value={email}
+              onChange={(e) => setEmail(e.currentTarget.value)}
+              required
+            />
+            <PasswordInput
+              label="Password"
+              placeholder="Enter Password"
+              value={password}
+              onChange={(e) => setPassword(e.currentTarget.value)}
+              required
+            />
+            <Button type="submit" fullWidth mt="lg">
+              Login
+            </Button>
+          </Stack>
+        </form>
+      </Card>
+    </div>
   );
 }
