01c06628aa3049014eaf26ff6aa1dd4c411105bb
Yoonho Kim <CAKIMYOO@de.eu.sew>
2025-05-06 08:57:23 -0400

    updated modal component so that 'cancel' and 'submit' buttons close the modal as well

--- src/app/@modal/(.)create/[id]/page.tsx
+++ src/app/@modal/(.)create/[id]/page.tsx
@@ -3,13 +3,22 @@
 import React from "react";
 import { useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
-import { Modal, Textarea, Button } from "@mantine/core";
+import { Modal, Textarea, Button, Group } from "@mantine/core";
 
 export default function ModalCreateQueryPage() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const label = searchParams.get("label");
   const [queryText, setQueryText] = useState("");
+
+  const handleClose = () => {
+    router.back();
+  };
+
+  const handleSubmit = () => {
+    // Need to add code to handle the submission of data
+    handleClose();
+  };
 
   return (
     <Modal
@@ -22,8 +31,10 @@ export default function ModalCreateQueryPage() {
         value={queryText}
         onChange={(e) => setQueryText(e.currentTarget.value)}
       />
-      <Button>Cancel</Button>
-      <Button>Submit</Button>
+      <Group mt="md">
+        <Button onClick={handleClose}>Cancel</Button>
+        <Button onClick={handleSubmit}>Submit</Button>
+      </Group>
     </Modal>
   );
 }
